6c4fb51a117a8a2b8b77c1eb3cc92cd6bcbfed93
DevFC-M <felipech3344@gmail.com>
2023-05-18 23:01:04 -0300

    Criando a pagina principal da aplicação, estilizações e bg's

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
 export default function Home() {
-  return <div className="text-2xl mt-10 font-bold uppercase w-20 h-20 bg-red-500 rounded">Oi</div>;
+  return (
+    <main className="grid grid-cols-2 min-h-screen">
+      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10 ">
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-700 -translate-y-1/2 translate-x-1/2 rounded-full blur-full" />
+        <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes" />{" "}
+      </div>
+
+      <div className="flex flex-col p-16 bg-[url(../assets/bg-star.svg)] bg-cover">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="text-center leading-relaxed w-[360px]">
+            Você ainda não registrou nenhuma lembrança, comece a{" "}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora
+            </a>
+            !
+          </p>
+        </div>
+      </div>
+    </main>
+  );
 }
